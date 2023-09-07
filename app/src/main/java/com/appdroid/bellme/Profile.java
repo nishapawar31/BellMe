576eab5f97dc50b98dc77a522d5152ff3261627f
NISHA <nishapawar3120@gmail.com>
2023-09-07 11:16:43 +0530

    Initial commit by nisha

---
+++ app/src/main/java/com/appdroid/bellme/Profile.java
@@ -0,0 +1,14 @@
+package com.appdroid.bellme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+}
